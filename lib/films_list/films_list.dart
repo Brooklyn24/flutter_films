8a226a82e11a9e5a966e4986623dab0550918890
Andrey Timofeev <atimofeev1992@gmail.com>
2019-01-04 14:14:01 +0700

    Create film page

---
+++ lib/films_list/films_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FilmsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Films"),
+      ),
+      body: Center(
+        child: Text("Body"),
+      ),
+    );
+  }
+}
